0776ebba71ab1f660f6380705d355e9fc57e48c3
Josh Bailey <josh@vandervecken.com>
2026-02-04 16:30:26 +1300

    restart.

--- restartit.sh
+++ restartit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cd $(dirname $0)
+cd $(dirname $0)/gamutrf
 export VOL_PREFIX=/scratch/gamutrf 
 export NEST=$(hostname)
 docker compose -f orchestrator.yml -f torchserve-orin.yml down
